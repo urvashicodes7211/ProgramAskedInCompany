dcabd187e9292d82828ff5bd69b7bb6dcc4a64c9
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-10 18:53:40 +0530

    Print a Pyramid Star Pattern

---
+++ E32.c
@@ -0,0 +1,20 @@
+/*Print a Pyramid Star Pattern 
+ 
+Write a program to print a pyramid pattern of stars (*) using a for loop. */
+
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("Enter n : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}

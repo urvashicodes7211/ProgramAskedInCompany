7ed2d63efb65ac3cf3eb975a8d0fc1bf7fb9b2c2
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-11 19:01:32 +0530

    sum of elements in Principal and Secondary diagonals

---
+++ E49.c
@@ -0,0 +1,31 @@
+/*Given a 2D square matrix, find the sum of elements in Principal and Secondary diagonals. For example, 
+consider the following 4 X 4 input matrix.*/
+
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("Enter n (square matrix) : ");
+    scanf("%d",&n);
+    int a[n][n];
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("Enter num : ");
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int sum = 0 , result = 0;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(i==j){
+                sum = sum + a[i][j];
+            }
+            if(i+j == n-1){
+                result = result + a[i][j];
+            }
+        }
+    }
+    printf("Principal Diagonal : %d\n",sum);
+    printf("Secondary Diagonal : %d",result);
+
+    return 0;
+}

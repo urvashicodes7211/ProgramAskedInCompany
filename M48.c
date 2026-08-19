d197659994bed9ce5c0833cd60a33be9d32df169
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-19 17:46:38 +0530

    M48 added

--- M48.c
+++ M48.c
@@ -7,6 +7,30 @@ int main(){
     printf("Enter m and n : ");
     scanf("%d %d",&m,&n);
     int a[m],b[n];
-    
+    int c[m+n];
+    for(int i=0;i<m;i++){
+        printf("Enter num : ");
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        printf("Enter num : ");
+        scanf("%d",&b[i]);
+    }
+    for(int i=0;i<m;i++){
+        c[i]=a[i];
+    }
+    for(int i=0;i<n;i++){
+        c[i+m]=b[i];
+    }
+    for(int i=0;i<m+n;i++){
+        for(int j=i+1;j<m+n;j++){
+            if(c[i] == c[j]){
+                c[j] = 0;
+            }
+        }
+    }
+    for(int i=0;i<m+n;i++){
+        printf("%d ",c[i]);
+    }
     return 0;
 }
